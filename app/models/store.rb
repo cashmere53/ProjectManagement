43c27e7a8a66265927d1d1468a10859cadb37931
Yu-Nishi <oishiimonogatabetai@gmail.com>
2015-12-15 14:37:53 +0900

    編集完了や登録完了にポップアップをつけた

--- app/models/store.rb
+++ app/models/store.rb
@@ -3,13 +3,16 @@ class Store < ActiveRecord::Base
   has_many :Housing, dependent: :destroy
 
   validates :store_name,
-            presence: { message: "⚠店舗名が入力されていません" }
+            presence: { message: "⚠店舗名が入力されていません" },
+            length: {maximum: 50, message: "⚠︎50文字以下で入力してください" }
+
 
   validates :street_address,
-            presence: { message: "⚠住所が入力されていません" }
+            presence: { message: "⚠住所が入力されていません" },
+            length: {maximum: 50, message: "⚠︎50文字以下で入力してください" }
 
   validates :postal_code,
             presence: { message: "⚠郵便番号が入力されていません" },
-            length: {maximum: 7, message: "⚠︎郵便番号は数字のみ7文字で入力してください" },
+            length: {in: 7..7, message: "⚠︎郵便番号は数字のみ7文字で入力してください" },
             numericality: { :only_integer => true, message: "⚠︎数字のみの形式で入力してください(XXX-YYYY→XXXYYYY)" }
 end
