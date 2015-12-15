41f45d8be74ba522d46802d5f9a2207cd5319e4c
Yu-Nishi <oishiimonogatabetai@gmail.com>
2015-12-15 15:46:13 +0900

    validate画像フォーマットがまだ それ以外の広告、店舗は完了 住宅のvalidateはまだ

--- app/models/advertising.rb
+++ app/models/advertising.rb
@@ -1,6 +1,12 @@
 class Advertising < ActiveRecord::Base
   belongs_to :inc_account
 
+  validates :advertise_info,
+            length: {maximum: 50, message: "⚠︎50文字以下で入力してください" }
+
   validates :image,
             presence: { message: "⚠画像が選択されていません" }
+
+  validates_format_of :image, :with => %r{\.(png|jpg|jpeg)$}i, :message => "画像フォーマットが適切ではありません", :multiline => true
+
 end
